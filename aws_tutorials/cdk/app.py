85f0216e9e6c3c6241ada330538d9c3a279ecdec
Rasmus Siljander <rasmus.siljander@aalto.fi>
2024-01-22 14:18:32 +0200

    code linting flake8 + black

--- aws_tutorials/cdk/app.py
+++ aws_tutorials/cdk/app.py
@@ -1,13 +1,10 @@
-from aws_cdk import (
-    aws_iam as iam,
-    core
-)
+import yaml
+
+from aws_cdk import aws_iam as core
 from s3_stack import S3Stack
-import yaml
 
-import yaml
 
-with open('env.yaml', 'r') as file:
+with open("env.yaml", "r") as file:
     config = yaml.safe_load(file)
 
 app = core.App()
--- aws_tutorials/cdk/s3_stack.py
+++ aws_tutorials/cdk/s3_stack.py
@@ -1,20 +1,17 @@
-from aws_cdk import (
-    aws_iam as iam,
-    core
-)
+from aws_cdk import aws_iam as iam, core
+
 
 class S3Stack(core.Stack):
     def __init__(self, config, scope: core.Construct, id: str) -> None:
         super().__init__(scope, id)
-
 
         self.arn_base = f"arn:aws:iam::{config['arn_number']}"
 
         # Define the IAM role
         my_role = iam.Role(
             self,
-            config['role_name'],
-            role_name=config['role_name'],
-            description='Role for S3 practice',
-            assumed_by=iam.ArnPrincipal(f"{self.arn_base}:root")
+            config["role_name"],
+            role_name=config["role_name"],
+            description="Role for S3 practice",
+            assumed_by=iam.ArnPrincipal(f"{self.arn_base}:root"),
         )
