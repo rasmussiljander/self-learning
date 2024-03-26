45db3cc9c645386bc2577ad8835a4423b680e413
Rasmus Siljander <47755039+rasmussiljander@users.noreply.github.com>
2024-03-26 18:51:05 +0200

    population eda

--- postalcode_analysis/data_exploration/utils.py
+++ postalcode_analysis/data_exploration/utils.py
@@ -16,3 +16,28 @@ def split_postcode(df: pd.DataFrame) -> pd.DataFrame:
     df.insert(1, "postcode", df["Postal code area"].str[:5])
     df = df.drop(columns=["Postal code area"])
     return df
+
+
+def normalize_by_population(
+    df: pd.DataFrame, population_col: str, skip_cols: list[str] = []
+) -> pd.DataFrame:
+    """
+    Normalize the columns of a DataFrame by dividing each column by the corresponding population value.
+
+    Args:
+        df (pd.DataFrame): The DataFrame to be normalized.
+        population_col (str): The name of the column containing population values.
+        skip_cols (list[str], optional): A list of column names to skip normalization. Defaults to [].
+
+    Returns:
+        pd.DataFrame: The normalized DataFrame.
+    """
+    pop_vec = df[population_col]
+
+    skip_norm_cols = [population_col, *skip_cols]
+    norm_cols = df.columns[~df.columns.isin(skip_norm_cols)]
+
+    for col in norm_cols:
+        df[col] /= pop_vec
+
+    return df
